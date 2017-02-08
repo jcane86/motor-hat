8e1e1591e42afabca6370c1b6552197fdcdc9272
pepe_mbp <jcane86@gmail.com>
2017-02-08 00:40:58 +0000

    Added tests for constructor options.
    Signed-off-by: pepe_mbp <jcane86@gmail.com>

--- test/index.js
+++ test/index.js
@@ -7,10 +7,42 @@ describe('motor-hat', function () {
     assert(typeof motorHat === 'function', 'motorHat is not of type "function"');
   });
 
-  it('should respect max number of motors', function () {
+  it('should respect max number of steppers', function () {
     assert.throws(function () {
       motorHat({
-        steppers: [1, 2, 3, 4, 5]
+        steppers: [1, 2, 3]
+      });
+    }, Error, 'Error thrown!');
+  });
+
+  it('should respect max number of DC motors', function () {
+    assert.throws(function () {
+      motorHat({
+        dc: [1, 2, 3, 4, 5]
+      });
+    }, Error, 'Error thrown!');
+  });
+
+  it('should respect max number of servos', function () {
+    assert.throws(function () {
+      motorHat({
+        servos: [1, 2, 3, 4, 5]
+      });
+    }, Error, 'Error thrown!');
+  });
+
+  it('should respect frequency type', function () {
+    assert.throws(function () {
+      motorHat({
+        frequency: 'string'
+      });
+    }, Error, 'Error thrown!');
+  });
+
+  it('should respect I2C Address type', function () {
+    assert.throws(function () {
+      motorHat({
+        address: 'string'
       });
     }, Error, 'Error thrown!');
   });
