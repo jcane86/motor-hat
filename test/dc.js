eecf72eaec078d154c410c54a038c2ab923e62ed
pepe_mbp <jcane86@gmail.com>
2017-09-08 19:14:28 +0100

    feat(lib/dc.js): Add Async support to DC motors
    All sync versions of pwm.js methods have been renamed to *Sync. Previous methods were transformed to
    
    BREAKING CHANGE: dcjs methods are now asynchronous and expect a callback as last parameter.
    
    Re #29

--- test/dc.js
+++ test/dc.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const sinon = require('sinon');
-require('should');
+const should = require('should');
 require('should-sinon');
 const dc = require('../lib/dc.js');
 const i2c = require('./stubi2c.js');
@@ -22,92 +22,185 @@ describe('lib/dclib.js', () => {
 
   it('should require options object to initialize', () => {
     (function () {
-      dc();
+      dc().init();
     }).should.throw();
   });
 
   it('should require 3 pins to initialize', () => {
     (function () {
-      dc({ pwm: {} });
+      dc({ pwm: {} }).init();
     }).should.throw();
 
     (function () {
-      dc({ pwm: {}, pins: [0, 1] });
+      dc({ pwm: {}, pins: [0, 1] }).init();
     }).should.throw();
 
     (function () {
-      dc({ pwm: {}, pins: [0, 1, 2, 4] });
+      dc({ pwm: {}, pins: [0, 1, 2, 4] }).init();
     }).should.throw();
   });
 
   it('should require pins in [0 to 15]', () => {
     (function () {
-      dc({ pwm: {}, pins: [16, 1, 2] });
+      dc({ pwm: {}, pins: [16, 1, 2] }).init();
     }).should.throw();
 
     (function () {
-      dc({ pwm: {}, pins: [-1, 3, 4] });
+      dc({ pwm: {}, pins: [-1, 3, 4] }).init();
     }).should.throw();
   });
 
   it('should require pwm instance to initialize', () => {
     (function () {
-      dc({ pins: ports.M1 });
+      dc({ pins: ports.M1 }).init();
     }).should.throw();
   });
 
-  it('should initialize', () => {
+  it('should initialize synchronously', () => {
     (function () {
-      dc({ pwm, pins: ports.M1 });
+      dc({ pwm, pins: ports.M1 }).init();
     }).should.not.throw();
 
     (function () {
-      dc({ pwm, pins: [8, 10, 9] });
+      dc({ pwm, pins: [8, 10, 9] }).init();
     }).should.not.throw();
   });
 
-  it('should respect default frequency', () => {
-    const stub = sinon.stub(pwm, 'setPWMFreq').callsFake(() => {});
-    dc({ pwm, pins: ports.M1 });
+  it('should initialize asynchronously with obj', (done) => {
+    (function () {
+      dc({ pwm, pins: ports.M1 }).init((err) => {
+        should.equal(err, null);
+        done();
+      });
+    }).should.not.throw();
+  });
+
+  it('should initialize asynchronously with arr', (done) => {
+    (function () {
+      dc({ pwm, pins: [8, 10, 9] }).init((err) => {
+        should.equal(err, null);
+        done();
+      });
+    }).should.not.throw();
+  });
+
+  it('should respect default frequency on synch init', () => {
+    const stub = sinon.stub(pwm, 'setPWMFreqSync').callsFake(() => {});
+    dc({ pwm, pins: ports.M1 }).init();
     stub.should.be.calledWith(1600);
     stub.restore();
   });
 
-  it('should respect default speed', () => {
-    const stub = sinon.stub(pwm, 'setPWM').callsFake(() => {});
-    dc({ pwm, pins: ports.M1 });
+  it('should respect default frequency on asynch init', (done) => {
+    const stub = sinon.stub(pwm, 'setPWMFreq').yieldsAsync(null);
+    dc({ pwm, pins: ports.M1 }).init((err) => {
+      should.equal(err, null);
+      stub.should.be.calledWith(1600);
+      stub.restore();
+      done();
+    });
+  });
+
+  it('should respect default speed on synch init', () => {
+    const stub = sinon.stub(pwm, 'setPWMSync').callsFake(() => {});
+    dc({ pwm, pins: ports.M1 }).init();
     stub.should.be.calledWith(8, 0, 4080);
     stub.restore();
   });
 
-  it('should change speed', () => {
-    const stub = sinon.stub(pwm, 'setPWM').callsFake(() => {});
-    dc({ pwm, pins: ports.M1 }).setSpeed(50);
+  it('should respect default speed on asynch init', (done) => {
+    const stub = sinon.stub(pwm, 'setPWM').yieldsAsync(null);
+    dc({ pwm, pins: ports.M1 }).init((err) => {
+      should.equal(err, null);
+      stub.should.be.calledWith(8, 0, 4080);
+      stub.restore();
+      done();
+    });
+  });
+
+  it('should change speed synchronously', () => {
+    const stub = sinon.stub(pwm, 'setPWMSync').callsFake(() => {});
+    dc({ pwm, pins: ports.M1 }).init().setSpeedSync(50);
     stub.should.be.calledWith(8, 0, 2040);
     stub.restore();
   });
 
-  it('should run forwards', () => {
-    const stub = sinon.stub(pwm, 'setPin').callsFake(() => {});
-    dc({ pwm, pins: ports.M1 }).run('fwd');
+  it('should change speed asynchronously', (done) => {
+    const stub = sinon.stub(pwm, 'setPWM').yieldsAsync(null);
+    dc({ pwm, pins: ports.M1 }).init((err, res) => {
+      should.equal(err, null);
+      res.setSpeed(50, (err) => {
+        should.equal(err, null);
+        stub.should.be.calledWith(8, 0, 2040);
+        stub.restore();
+        done();
+      });
+    });
+  });
+
+  it('should run forwards synchronously', () => {
+    const stub = sinon.stub(pwm, 'setPinSync').callsFake(() => {});
+    dc({ pwm, pins: ports.M1 }).init().runSync('fwd');
     stub.should.be.calledWith(10, 1);
     stub.should.be.calledWith(9, 0);
     stub.restore();
   });
 
-  it('should run back', () => {
-    const stub = sinon.stub(pwm, 'setPin').callsFake(() => {});
-    dc({ pwm, pins: ports.M1 }).run('back');
+  it('should run forwards asynchronously', (done) => {
+    const stub = sinon.stub(pwm, 'setPin').yieldsAsync(null);
+    dc({ pwm, pins: ports.M1 }).init((err, res) => {
+      should.equal(err, null);
+      res.run('fwd', (err) => {
+        should.equal(err, null);
+        stub.should.be.calledWith(10, 1);
+        stub.should.be.calledWith(9, 0);
+        stub.restore();
+        done();
+      });
+    });
+  });
+
+  it('should run back synchronously', () => {
+    const stub = sinon.stub(pwm, 'setPinSync').callsFake(() => {});
+    dc({ pwm, pins: ports.M1 }).init().runSync('back');
     stub.should.be.calledWith(10, 0);
     stub.should.be.calledWith(9, 1);
     stub.restore();
   });
 
-  it('should stop', () => {
-    const stub = sinon.stub(pwm, 'setPin').callsFake(() => {});
-    dc({ pwm, pins: ports.M1 }).stop();
+  it('should run back asynchronously', (done) => {
+    const stub = sinon.stub(pwm, 'setPin').yieldsAsync(null);
+    dc({ pwm, pins: ports.M1 }).init((err, res) => {
+      should.equal(err, null);
+      res.run('back', (err) => {
+        should.equal(err, null);
+        stub.should.be.calledWith(10, 0);
+        stub.should.be.calledWith(9, 1);
+        stub.restore();
+        done();
+      });
+    });
+  });
+
+  it('should stop synchronously', () => {
+    const stub = sinon.stub(pwm, 'setPinSync').callsFake(() => {});
+    dc({ pwm, pins: ports.M1 }).init().stopSync();
     stub.should.be.calledWith(10, 0);
     stub.should.be.calledWith(9, 0);
     stub.restore();
+  });
+
+  it('should stop asynchronously', (done) => {
+    const stub = sinon.stub(pwm, 'setPin').yieldsAsync(null);
+    dc({ pwm, pins: ports.M1 }).init((err, res) => {
+      should.equal(err, null);
+      res.stop((err) => {
+        should.equal(err, null);
+        stub.should.be.calledWith(10, 0);
+        stub.should.be.calledWith(9, 0);
+        stub.restore();
+        done();
+      });
+    });
   });
 });
